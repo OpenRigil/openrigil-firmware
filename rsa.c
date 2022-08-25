bdfe28fc09b0b39bf367397afb91ea61eb43aed9
Zenithal <i@zenithal.me>
2022-08-25 10:03:07 +0800

    Disable get public key

--- rsa.c
+++ rsa.c
@@ -6,23 +6,28 @@
 int rsa2048_get_public_key(rsa_key_t *key, uint8_t *n);
 int rsa4096_get_public_key(rsa_key_t *key, uint8_t *n);
 
-int rsa_get_public_key(rsa_key_t *key, uint8_t *n) {
-    if (key->nbits == 2048) {
-        return rsa2048_get_public_key(key, n);
-    } else if (key->nbits == 4096) {
-        return rsa4096_get_public_key(key, n);
-    }
-    return 0;
-}
+// this does not work for now
+//int rsa_get_public_key(rsa_key_t *key, uint8_t *n) {
+//    if (key->nbits == 2048) {
+//        rsa2048_get_public_key(key, n);
+//        return 0;
+//    } else if (key->nbits == 4096) {
+//        rsa4096_get_public_key(key, n);
+//        return 0;
+//    }
+//    return 0;
+//}
 
 int rsa2048_private(rsa_key_t *key, const uint8_t *input, uint8_t *output);
 int rsa4096_private(rsa_key_t *key, const uint8_t *input, uint8_t *output);
 
 int rsa_private(rsa_key_t *key, const uint8_t *input, uint8_t *output) {
     if (key->nbits == 2048) {
-        return rsa2048_private(key, input, output);
+        rsa2048_private(key, input, output);
+        return 0;
     } else if (key->nbits == 4096) {
-        return rsa4096_private(key, input, output);
+        rsa4096_private(key, input, output);
+        return 0;
     }
     return 0;
 }
